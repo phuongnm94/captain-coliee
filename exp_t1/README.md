8a88ffbd1bee421401c534b1ce8e60d9dc3cca17
Minh Nguyen <minh.nn26700@gmail.com>
2024-06-14 14:14:39 +0900

    Update 'README.md' for more detailed directory description and runs.

--- exp_t1/README.md
+++ exp_t1/README.md
@@ -1,33 +1,67 @@
-# Prepare the data directory:
+
+---
+
+## Data
+Please visit [COLIEE 2023](https://sites.ualberta.ca/~rabelo/COLIEE2024/) for whole dataset request.
+
+Structure of data directory:
 ```
-dataset
-    - bm25 // to store bm25 indices
-    - c2023
-        - test_files
-        - train_files
-        - test_no_labels.json
-        - train_labels.json
-    - c2024
-        - test_files
-        - train_files
-        - test_no_labels.json
-        - train_labels.json
-    - json
-    - processed // store processed documents
-    - queries // store extracted TF-IDF keywords using as queries
-    - summarized
+.
+└── dataset/
+    ├── bm25/
+    │   ├── corpus
+    │   └── index
+    ├── c2023/
+    │   ├── test_files/
+    │   │   ├── 000371.txt
+    │   │   └── ...
+    │   ├── train_files/
+    │   │   ├── 000028.txt
+    │   │   └── ...
+    │   ├── test_no_labels.json
+    │   ├── train_labels.json
+    │   ├── bm25_candidates_train.json
+    │   ├── bm25_candidates_dev.json
+    │   └── bm25_candidates_test.json
+    ├── c2024/
+    │   ├── test_files/
+    │   │   ├── 000371.txt
+    │   │   └── ...
+    │   ├── train_files/
+    │   │   ├── 000028.txt
+    │   │   └── ...
+    │   ├── test_no_labels.json
+    │   └── train_labels.json
+    ├── processed/
+    │   ├── 000028.txt
+    │   └── ...
+    ├── queries/
+    │   ├── 000028.txt
+    │   └── ...
+    └── summarized/
+        ├── 000028.txt
+        └── ...
 ```
 
-# Mono T5
+## Environments
+```bash
+conda create -n env_coliee_t1 python=3.10.13
+conda activate env_coliee_t1
+pip install requirements.txt
+```
+
+## All runs
+Overall the results can be reproduced by running:
 1. Run `python process.py` to generate processed documents.
 2. Run `python generate_query.py --n_keywords 25` to generate TF-IDF keywords for each document.
-3. Split train, dev, test in the file `split_data.ipynb.`
-4. Check out bm25 result in `test_bm25.ipynb`
-5. Using LLMs to get summary of the documents in `summary.ipynb`
+3. Run `split_data.py` to split train, dev, test in the file
+4. Run `test_bm25.py` to checkout BM25's results
+5. Using LLMs to get summary of the documents using FLanT5 in `summary.ipynb`. To get the Mistral summary, run `get_mistrial_summarize.py`.
 6. Finetune  monot5 by running `train.sh` or `python train_monot5.py --config ./configs/monot5-large-10k_hns.json` 
-7. Eval monot5: run the `mono_t5.ipynb` notebook
+7. To evaluate results, run the `mono_t5.ipynb` notebook
 
-Results:
+## Results
+```
 - Top k = 150:
     - bm25 recall 0.8231
     - mono-t5: (f1, precision, recall)
@@ -38,6 +72,6 @@ Results:
     - mono-t5: (f1, precision, recall)
         - dev: 0.3534, 0.3578, 0.3492
         - test: 0.2372, 0.1779, 0.3558
-
+```
 # LLMs pair-wise
 Check out the `infer_llm.ipynb` notebook.
---
+++ exp_t1/test_bm25.py
@@ -0,0 +1,239 @@
+import numpy as np
+import pandas as pd
+import json
+import pickle
+import torch
+import nltk
+import os
+import re
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from rank_bm25 import BM25Okapi
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+def load_data(dir):
+    with open(dir, 'r') as fp:
+        train_data = json.load(fp)
+
+    data = []
+    for key in train_data.keys():
+        data.append([key, train_data[key]])
+
+    return pd.DataFrame(data, columns=['source', 'target'])
+
+def chunking(sentences, window_size=10):
+    chunks = []
+    for i in range(0, len(sentences) - window_size, window_size//2):
+        chunks.append("\n".join(sentences[i:i+window_size]))
+    return chunks
+
+with open('dataset/all_data.json') as f:
+    all_data_dict = json.load(f)
+
+word_tokenizer = nltk.tokenize.WordPunctTokenizer()
+# file_list = sorted(list(all_data_dict.keys()))
+
+file_list = [f for f in os.listdir('dataset/c2023/test_files') if f.endswith('.txt')]
+file_list = [f for f in file_list if f in all_data_dict.keys()]
+file_list = sorted(file_list)
+
+processed_file_dict = {}
+for file in [f for f in os.listdir('dataset/processed') if not f.startswith('.')]:
+    processed_file = f"dataset/processed/{file}"
+    with open(processed_file, 'r') as fp:
+        processed_document = fp.read()
+        processed_file_dict[file] = {
+            'sentences': processed_document.split('\n\n'),
+            'processed_document': processed_document
+        }
+
+chunk_dict = {}
+for file in file_list:
+    chunks = chunking(processed_file_dict[file]['sentences'])
+    for i, chunk in enumerate(chunks):
+        if len(chunk) > 0:
+            chunk_dict[f"{file}_{i}"] = chunk
+
+mode = 'document'
+if mode == 'chunk':
+    # bm25 for chunks
+    corpus = []
+    chunk_list = sorted(list(chunk_dict.keys()))
+    for chunk in chunk_list:
+        corpus.append(chunk_dict[chunk])
+    tokenized_corpus = [word_tokenizer.tokenize(doc) for doc in corpus]
+    bm25 = BM25Okapi(tokenized_corpus)
+else:
+    # bm25 for whole document
+    corpus = []
+    prcessed_list = sorted(file_list)
+    for file in prcessed_list:
+        corpus.append(processed_file_dict[file]['processed_document'])
+    tokenized_corpus = [word_tokenizer.tokenize(doc) for doc in corpus]
+    bm25 = BM25Okapi(tokenized_corpus)
+    
+stopwords = nltk.corpus.stopwords.words('english')
+
+docs = []
+for file in processed_file_dict.keys():
+    docs.append(processed_file_dict[file]['processed_document'])
+
+count_vec = CountVectorizer(stop_words=stopwords)
+word_count_vector = count_vec.fit_transform(docs)
+
+
+tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+tfidf_transformer.fit(word_count_vector)
+
+features = count_vec.get_feature_names_out()
+
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    """get the feature names and tf-idf score of top n items"""
+    
+    #use only topn items from vector
+    sorted_items = sorted_items[:topn]
+
+    score_vals = []
+    feature_vals = []
+    
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+        
+        #keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+
+    #create a tuples of feature,score
+    #results = zip(feature_vals,score_vals)
+    results= {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]]=score_vals[idx]
+    
+    return results
+
+
+# # Query on chunks
+# n_keywords = 25
+
+# def extract_query(doc):
+#     tf_idf_vector=tfidf_transformer.transform(count_vec.transform([doc]))
+#     sorted_items = sort_coo(tf_idf_vector.tocoo())
+#     keywords=extract_topn_from_vector(features,sorted_items, n_keywords)
+#     return " ".join(list(keywords.keys()))
+
+# query_dict = {}
+
+# for file in tqdm(file_list):
+#     query_dict[file] = extract_query(processed_file_dict[file]['processed_document'])
+
+# n_candidates = 50
+# chunk_candidate_dict = {}
+# candidate_dict = {}
+
+# for file in tqdm(file_list):
+#     query = query_dict[file]
+#     tokenized_query = word_tokenizer.tokenize(query)
+#     results = bm25.get_scores(tokenized_query)
+#     max_ids = np.argsort(results)[-n_candidates:]
+
+#     chunk_candidates = [chunk_list[idx] for idx in max_ids]
+#     chunk_candidate_dict[file] = chunk_candidates
+
+#     document_candidates = [chunk.split('_')[0] for chunk in chunk_candidates]
+#     candidate_dict[file] = list(set(document_candidates))
+    
+
+# test_df = load_data('dataset/json/test.json')
+
+# test_df['chunk_candidates'] = test_df['source'].apply(lambda x: chunk_candidate_dict[x])
+# test_df['candidates'] = test_df['source'].apply(lambda x: candidate_dict[x])
+# test_df['query'] = test_df['source'].apply(lambda x: query_dict[x])
+
+# # calculate accuracy metrics for BM25 + TF-IDF
+# correct = 0
+# n_retrived = 0
+# n_relevant = 0
+
+# coverages = []
+
+# for index, row in test_df.iterrows():
+#     source = row['source']
+#     target = row['target']
+#     preds = row['candidates']
+#     coverages.append(len(preds))
+#     n_retrived += len(preds)
+#     n_relevant += len(target)
+#     for prediction in preds:
+#         if prediction in target:
+#             correct += 1
+
+# precision = correct / n_retrived
+# recall = correct / n_relevant
+
+# print(f"Average # candidates: {np.mean(coverages)}")
+# print(f"Precision: {precision}")
+# print(f"Recall: {recall}")
+# print(f"F1: {2 * precision * recall / (precision + recall)}")
+
+
+# Query on whole document
+
+n_keywords = 25
+
+def extract_query(doc):
+    tf_idf_vector=tfidf_transformer.transform(count_vec.transform([doc]))
+    sorted_items = sort_coo(tf_idf_vector.tocoo())
+    keywords=extract_topn_from_vector(features,sorted_items, n_keywords)
+    return " ".join(list(keywords.keys()))
+
+query_dict = {}
+
+for file in tqdm(file_list):
+    query_dict[file] = extract_query(processed_file_dict[file]['processed_document'])
+
+n_candidates = 50
+candidate_dicts = {}
+
+for file in tqdm(file_list):
+    query = query_dict[file]
+    tokenized_query = word_tokenizer.tokenize(query)
+    results = bm25.get_scores(tokenized_query)
+    max_ids = np.argsort(results)[-n_candidates:]
+    document_candidates = [file_list[idx] for idx in max_ids]
+    candidate_dicts[file] = list(set(document_candidates))
+
+test_df = load_data('dataset/test.json')
+
+test_df['candidates'] = test_df['source'].apply(lambda x: candidate_dicts[x])
+test_df['query'] = test_df['source'].apply(lambda x: query_dict[x])
+
+# calculate accuracy metrics for BM25 + TF-IDF
+correct = 0
+n_retrived = 0
+n_relevant = 0
+
+coverages = []
+
+for index, row in test_df.iterrows():
+    source = row['source']
+    target = row['target']
+    preds = row['candidates']
+    coverages.append(len(preds))
+    n_retrived += len(preds)
+    n_relevant += len(target)
+    for prediction in preds:
+        if prediction in target:
+            correct += 1
+
+precision = correct / n_retrived
+recall = correct / n_relevant
+
+print(f"Average # candidates: {np.mean(coverages)}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1: {2 * precision * recall / (precision + recall)}")
