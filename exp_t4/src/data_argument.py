d33f383161e3736b9579a0366c06b4b4d8d245fb
trieuhoangan <trieuhoangan1012@gmail.com>
2025-06-08 18:08:57 +0900

    refactor data argument file

--- exp_t4/src/data_argument.py
+++ exp_t4/src/data_argument.py
@@ -1,125 +1,18 @@
 from os import listdir
 from os.path import isfile, join
-
-def get_all_files_from_path(mypath):
-    filenames = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
-    return filenames
-
+import utils.utils as ult
 from bs4 import BeautifulSoup
 import re
 import json
-
-def get_article(articles):
-    result = {}
-    current_statue = "(non-statute)"
-    for i in re.split(r"(.*)", articles.strip()):
-        if len(i) == 0 or i == "\n":
-            continue
-        if re.search(r"^\(.*\)$", i):
-            current_statue = i.strip()
-            if current_statue not in result:
-                result.update({current_statue: []})
-        else:
-            if current_statue not in result:
-                result.update({current_statue: []})
-            result[current_statue].append(i)
-    return result
-
-def build_test(filename):
-    result = {}
-    with open(filename, 'r') as f:
-        data = f.read()
-
-    data = BeautifulSoup(data, "xml").find_all('pair')
-    for i in data:
-        id = i.get('id')
-        result.update({id: {}})
-        result[id].update({"label": i.get('label')})
-        articles = i.find('t1').text.strip()
-        # articles = get_article(articles)
-        result[id].update({"result": articles})
-        result[id].update({"content": i.find('t2').text.strip()})
-    return result
-
-def write_json(filename, data):
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-import xml.etree.ElementTree as Et
-import glob
-
-def format_first_line(text):
-    lines = text.split("\n")
-    results = []
-    for line in lines:
-        if line[0] == "":
-            continue
-        if line[0] == "(" and line[-1] == ")":
-            continue
-        results.append(line)
-    return "\n".join(results)
-
-def load_samples(filexml):
-    # try:
-        tree = Et.parse(filexml)
-        root = tree.getroot()
-        samples = []
-        for i in range(0, len(root)):
-            sample = {'result': []}
-            for j, e in enumerate(root[i]):
-                if e.tag == "t1":
-                    sample['result'] = format_first_line(e.text.strip())
-                elif e.tag == "t2":
-                    question = e.text.strip()
-                    sample['content'] = question if len(question) > 0 else None
-            sample.update(
-                {'index': root[i].attrib['id'], 'label': root[i].attrib.get('label', "N")})
-            # filter the noise samples
-            if sample['content'] is not None:
-                samples.append(sample)
-            else:
-                print("[Important warning] samples {} is ignored".format(sample))
-        return samples
-
-def load_test_data_samples(path_folder_base, test_id):
-    data = []
-    test = load_samples(f"{path_folder_base}/riteval_{test_id}.xml")
-    for file_path in glob.glob(f"{path_folder_base}/riteval_{test_id}.xml"):
-        data = data + load_samples(file_path)
-    return data
-
-
-def load_all_data_samples(path_folder_base):
-    data = []
-    for file_path in glob.glob("{}/*.xml".format(path_folder_base)):
-        data = data + load_samples(file_path)
-    return data
-
-def check_false_labels(pred, false_labels):
-	for label in false_labels:
-		if label in pred:
-			return True
-	return False
-
 from tqdm import tqdm
-
-def format_output(text):
-	CLEANR = re.compile('<.*?>') 
-	cleantext = re.sub(CLEANR, '', text)
-	return cleantext.strip().lower()
-
-def readfile(filename):
-    f = open(filename)
-    data = json.load(f)
-    return data
-
-def load_jsonl(file):
-    with open(file) as f:
-        data = [json.loads(line) for line in f]
-    return data
-
-from transformers import AutoTokenizer, BloomForCausalLM, AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, BloomForCausalLM, AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
+from transformers.generation import GenerationConfig
 import torch
+import random
+import copy
+import jsonlines
+import argparse
+import ujson
 
 model_name = "google/flan-t5-xxl"
 cache_dir = ".cache"
@@ -128,12 +21,6 @@ model = AutoModelForSeq2SeqLM.from_pretrained(
 		model_name, device_map="auto", cache_dir=cache_dir, torch_dtype=torch.float16, load_in_8bit=True
 	)
 
-from tqdm import tqdm
-
-def format_output(text):
-	CLEANR = re.compile('<.*?>') 
-	cleantext = re.sub(CLEANR, '', text)
-	return cleantext.strip()#.lower()
 
 def prompting(premise, hypothesis, label, template=None):
     if "true" in template.lower():
@@ -153,16 +40,15 @@ def write_cot(result, filename):
         for line in data:
             f.write(line+'\n')
 
-list_prompt = readfile("../data/prompt4.json")
+list_prompt = ult.readfile("../data/prompt4.json")
 list_prompt[0]["prompt"] = 'Summarize this sentence: "{{premise}}"\nApproach: Issue, rule, application, conclusion.'
-list_prompt
+# list_prompt
 
-import copy
 
 def predict(model, tokenizer, files=["riteval_R01_en","riteval_R02_en","riteval_R03_en","riteval_R04_en"], output="../output/cot/newpromt_"):
     for file in files:
         test_file = path_file+file+".xml"
-        data = load_samples(test_file)
+        data = ult.load_samples(test_file)
         
         acc = {}
         for template_prompt in list_prompt:
@@ -178,7 +64,7 @@ def predict(model, tokenizer, files=["riteval_R01_en","riteval_R02_en","riteval_
                 text = prompting(premise, hypothesis, label, template_prompt)
                 inputs = tokenizer(text, return_tensors="pt")["input_ids"].cuda()
                 outputs = model.generate(inputs, max_new_tokens=256)
-                output_text = format_output(tokenizer.decode(outputs[0]).replace(text, "").split("\n")[-1])
+                output_text = ult.format_output(tokenizer.decode(outputs[0]).replace(text, "").split("\n")[-1])
                 item.update({"prompt": text})
                 item.update({"sum": output_text})
                 result.append(item)
@@ -196,12 +82,12 @@ query_encoder = SentenceTransformer('facebook-dpr-question_encoder-single-nq-bas
 passage_encoder = SentenceTransformer('facebook-dpr-ctx_encoder-single-nq-base')
 
 def dpr(testfile=None, path="../data/COLIEE2024statute_data-English/fewshot"):
-    datas = get_all_files_from_path(path)
+    datas = ult.get_all_files_from_path(path)
     corpus = []
     content = []
     labels = []
     for data in datas:
-        data = load_samples(data)
+        data = ult.load_samples(data)
         for item in data:
             # corpus.append(item["result"].replace("\n", " ").strip())
             corpus.append(item["result"].strip())
@@ -217,11 +103,7 @@ corpus, content, labels, retrival_passage_embeddings, content_passage_embeddings
 template = "Document: {{premise}}\nQuestion: {{hypothesis}}? True or False "
 path = "../output/sum/prompt_19"
 
-files = get_all_files_from_path(path)
-import torch
-import json
-import re
-import random
+files = ult.get_all_files_from_path(path)
 
 full_data = []
 for file in files:
@@ -310,20 +192,20 @@ for file in files:
         full_data.append(result3)
 
 # f = open("../output/sum/full_data.jsonl", "w", encoding="utf-8")
-import jsonlines
+
 random.shuffle(full_data)
 with jsonlines.open("../data/COLIEE2024statute_data-English/aug_sum/full_data.jsonl", mode='w') as writer:
     writer.write_all(full_data)
 
 
 path_file = "../data/COLIEE2024statute_data-English/fewshot.json/"
-files = get_all_files_from_path(path_file)
+files = ult.get_all_files_from_path(path_file)
 files
 
 articles = {}
 
 for file in files:
-    data = load_jsonl(file)
+    data = ult.load_jsonl(file)
     if "R05" in file: 
         for item in data:
             art = item["result"].split("\n")
@@ -366,11 +248,6 @@ for file in files:
 
 # articles
 
-
-def write_json(filename, data):
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
 f = open("../data/COLIEE2024statute_data-English/text/civil_code_en-1to724-2.txt", "r")
 
 articles_list = []
@@ -393,30 +270,44 @@ for line in f:
             result += line+" "
 
 f.close()
-articles
+# articles
 
-model_name = "Qwen/Qwen-72B-Chat"
-cache_dir = "/home/congnguyen/drive/.cache"
-# cache_dir = ".cache"
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.generation import GenerationConfig
-import torch
 
-# Note: The default behavior now has injection attack prevention off.
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-72B-Chat", cache_dir=cache_dir, trust_remote_code=True)
-# device_map="auto", 
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-72B-Chat", cache_dir=cache_dir, device_map="auto", 
-                                             torch_dtype=torch.float16, trust_remote_code=True, load_in_4bit=True).eval()
+    
+def main(args):
+    model_name = "Qwen/Qwen-72B-Chat"
+    cache_dir = "/home/congnguyen/drive/.cache"
+    # cache_dir = ".cache"
 
-prompt = "'{{text}}'. Analyze the structure following main premise, exception of each rule."
-result_art = {}
-# history = "Main Clause: A juridical person is not formed other than pursuant to the provisions of this Code or other laws.\nException Clauses:None"
-for art in articles:
-    result_art.update({art: "Error OOM!"})
-    text = prompt.replace("{{text}}", articles[art].strip())
-    try:
-        response, history = model.chat(tokenizer, text, history=None)
-        print(response)
-        result_art.update({art: response})
-    except:
-        write_json("../data/COLIEE2024statute_data-English/text/result_art.json", result_art)
+
+    # Note: The default behavior now has injection attack prevention off.
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-72B-Chat", cache_dir=cache_dir, trust_remote_code=True)
+    # device_map="auto", 
+    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-72B-Chat", cache_dir=cache_dir, device_map="auto", 
+                                                torch_dtype=torch.float16, trust_remote_code=True, load_in_4bit=True).eval()
+
+    prompt = "'{{text}}'. Analyze the structure following main premise, exception of each rule."
+    result_art = {}
+    # history = "Main Clause: A juridical person is not formed other than pursuant to the provisions of this Code or other laws.\nException Clauses:None"
+    for art in articles:
+        result_art.update({art: "Error OOM!"})
+        text = prompt.replace("{{text}}", articles[art].strip())
+        try:
+            response, history = model.chat(tokenizer, text, history=None)
+            print(response)
+            result_art.update({art: response})
+        except:
+            ult.write_json("../data/COLIEE2024statute_data-English/text/result_art.json", result_art)
+        # return None
+    
+if __name__ =="__main__":
+    parser = argparse.ArgumentParser('_')
+    parser.add_argument('--model-name', type=str, required=True)
+    parser.add_argument('--cache-dir', type=str, required=False)
+    parser.add_argument('--data-path', type=str, required=True)
+    parser.add_argument('--list-prompt-path', type=str, required=False)
+    parser.add_argument('--cot-path', type=str, required=False)
+    parser.add_argument('--test-file-path', type=str, required=False)
+    parser.add_argument('--output-file-path', type=str, required=False)
+    parser.add_argument('--fewshot-path', type=str, required=True)
+    args = parser.parse_args()
